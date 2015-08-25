7561f306eea6ad723466c55d150ce3774ab27090
Varun Bhardwaj <vforvarun3@gmail.com>
2015-08-24 23:19:07 -0400

    Cleaned up.

--- src/com/DupSubStr.java
+++ src/com/DupSubStr.java
@@ -1,3 +1,6 @@
+// Varun Bhardwaj
+// August 22, 2015
+
 package com;
 
 import java.util.HashMap;
@@ -7,13 +10,14 @@ public class DupSubStr {
 
 	public static void main(String[] args) {
 		System.out.println(getDupSubStr("abcd"));
-		System.out.println(getDupSubStr("sffa"));
+		System.out.println(getDupSubStr("sfsda"));
 		System.out.println(getDupSubStr("a"));
 		System.out.println(getDupSubStr("aa"));
 		System.out.println(getDupSubStr("ab"));
-		System.out.println(getDupSubStr("sfhsdjfklsflkjsdlfjsldfj")); // bad ass test case
+		System.out.println(getDupSubStr("sfhsdjfklsflkjsdlfjsldfj")); // badass test case
 		System.out.println(getDupSubStr("sfsfjklsdajfsdjafkljsadlasdfa"));
-		System.out.println(getDupSubStr("GEEKSFORGEEKS"));	
+		System.out.println(getDupSubStr("GEEKSFORGEEKS"));
+
 	}
 	
 	public static String getDupSubStr(String str) {
@@ -22,23 +26,17 @@ public class DupSubStr {
 		int start = 0, end = 0, longestStart = 0, longestEnd = 0;
 		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 		for (int i = 0; i < charArr.length; i++) {
-			if (!map.containsKey(charArr[i])) {
-				map.put(charArr[i], 1);
-				end = i;
-				if (end-start > longestEnd - longestStart) {
-					longestStart = start;
-					longestEnd = end;
-				}
-			} else { // encountered a duplicate in string
-				if (end-start > longestEnd - longestStart) {
-					longestStart = start;
-					longestEnd = end;
-				}
-				// reset the pointers, clear older hashmap entries
-				start = i;
-				end   = i;
-				map.clear();
-				map.put(charArr[i], 1);
+			Character c = charArr[i];
+			if (map.containsKey(c) &&  (map.get(c) >= start)) {
+				start = map.get(charArr[i]) + 1;
+			}
+			
+			map.put(charArr[i], i);
+			end   = i;
+			
+			if (end-start > longestEnd - longestStart) {
+				longestStart = start;
+				longestEnd = end;
 			}
 		}
 		return str.substring(longestStart, longestEnd+1);
